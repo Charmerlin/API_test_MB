c1612ec2649e18c9405593a14084d37a478126e5
Charmerlin <153349993+Charmerlin@users.noreply.github.com>
2024-03-16 20:57:28 +0700

    Add files via upload

---
+++ lib/Beers_class.dart
@@ -0,0 +1,25 @@
+class Beers {
+  final String price;
+  final String name;
+  final BeerRating rating; // เปลี่ยนจาก List<double> เป็น BeerRating
+  final String image;
+  final int id;
+
+  Beers({
+    required this.price,
+    required this.name,
+    required this.rating,
+    required this.image,
+    required this.id,
+  });
+}
+
+class BeerRating {
+  final double average;
+  final int reviews;
+
+  BeerRating({
+    required this.average,
+    required this.reviews,
+  });
+}
